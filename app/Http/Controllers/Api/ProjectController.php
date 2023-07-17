b39e77ce5abc8351cb4939e776274dd5cb61dc99
Sofia Di Blasi <phia.di.blasi@gmail.com>
2023-07-17 16:12:02 +0100

    aggiunto controller api e testata chiamata postman

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(){
+
+        $projects = Project::all();
+
+        $response = [
+            "succes" => true,
+            "results" => $projects
+        ];
+
+        return response()->json($response);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/projects", function() {
-    return response()->json(
-       []
-    );
-});
+Route::get("/projects", [ProjectController::class, "index"]);
